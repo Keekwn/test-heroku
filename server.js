c060eb6007a9ea5378b1cf2bfaffeb47aa32f7e9
therock34 <ogimelih34@gmail.com>
2023-06-12 15:40:04 +0300

    Dosya güncellemesi

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const YAML = require('yamljs');
 const swaggerUi = require('swagger-ui-express');
+const path = require('path');
 
 const swaggerDocument = YAML.load(path.join(__dirname, 'openapi.yaml'));
 
@@ -8,36 +9,10 @@ const app = express();
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.post('/boapi/v1/farm', function(req, res) {
-  const data = req.body;  // Gelen veriyi al
-
-  // Verilerin doğruluğunu kontrol et ve gereken işlemleri yap
-  if (validateData(data)) {
-    saveData(data);  // Verileri kaydet
-    makePostRequest(data);  // POST isteği yap
-    res.status(200).json({ message: 'Veri başarıyla işlendi ve POST isteği yapıldı.' });
-  } else {
-    res.status(400).json({ message: 'Geçersiz veri.' });
-  }
+    // Burada POST isteği için işlem yapılır.
 });
 
-// Sunucunuzu başlatın
-const port = process.env.PORT || 5000;  // Heroku, dinlenmesi gereken portu belirtir
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on port ${port}`);
 });
-
-function validateData(data) {
-  // Gelen verilerin doğruluğunu kontrol et (örneğin, gereken alanlar var mı?)
-  // Doğrulama işlemlerini burada yap ve sonucu döndür
-  // Eğer veriler geçerli ise true, değilse false döndür
-}
-
-function saveData(data) {
-  // Gelen verileri kaydet (örneğin, veritabanına kaydet)
-}
-
-function makePostRequest(data) {
-  // POST isteğini yapacak olan fonksiyonu burada oluştur
-  // Verileri al ve belirtilen hedef URL'ye POST isteğini gerçekleştir
-  // İstek gönderildikten sonra, sonucu kontrol et ve gerekli işlemleri yap
-}
