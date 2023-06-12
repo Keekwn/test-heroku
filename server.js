4e42901037e7c18654c7a4e92cac89359e3af9ff
therock34 <ogimelih34@gmail.com>
2023-06-12 15:09:33 +0300

    Dosya güncellemesi

--- server.js
+++ server.js
@@ -1,19 +1,17 @@
-const express = require('express');
-const YAML = require('yamljs');
 const swaggerUi = require('swagger-ui-express');
-const fs = require('fs');
-
-const swaggerDocument = YAML.load('./openapi.yaml');
+const YAML = require('yamljs');
+const express = require('express');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
+// YAML dosyasını yükle
+const swaggerDocument = YAML.load('./OneDrive/Masaüstü/server/openapi.yaml');
+
+// Swagger UI'nin /api-docs yolunda görüntülenmesini sağla
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-app.post('/boapi/v1/farm', function(req, res) {
-    // POST isteği için burada işlemler yapılır.
-});
 
-// Sunucuyu başlat
-const port = process.env.PORT || 5000;  // Heroku tarafından dinlenmesi gereken portu belirtir
+// Server'ı başlat
 app.listen(port, () => {
-    console.log(`Server ${port} numaralı portta çalışıyor.`);
+  console.log(`Server ${port} numaralı portta çalışıyor.`);
 });
