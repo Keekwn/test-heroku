79496101da689843e9bf2310b23f3a1029127d97
therock34 <ogimelih34@gmail.com>
2023-06-12 22:25:07 +0300

    Değişikliklerinizi açıklayan bir mesaj

--- index.js
+++ index.js
@@ -16,11 +16,6 @@ var app = expressAppConfig.getApp();
 
 // Swagger middleware'ini başlatın
 http.createServer(app).listen(serverPort, function () {
-    console.log('Serverınız %d numaralı portta dinleniyor (http://localhost:%d)', serverPort, serverPort);
-    console.log('Swagger-ui, http://localhost:%d/docs adresinde kullanılabilir durumda', serverPort);
-    const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Server is up on port ${port}`);
-});
-
+    console.log('Serveriniz %d numarali portta dinleniyor (http://localhost:%d)', serverPort, serverPort);
+    console.log('Swagger-ui, http://localhost:%d/docs adresinde kullanilabilir durumda', serverPort);
 });
