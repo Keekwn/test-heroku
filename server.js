5922d122db276f5f04e130e0d3ecfe9aab92b3fd
therock34 <ogimelih34@gmail.com>
2023-06-12 20:32:09 +0300

    Fix: remove db usage in POST endpoint

--- server.js
+++ server.js
@@ -11,28 +11,22 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(express.json());
 
 app.post('/boapi/v1/farm', (req, res) => {
-    // Request body'den veriyi al
-    const { walletAddress, walletChainId, farmValueCent, farmDurationDays, farmPercentPer30Days } = req.body;
+    // Gelen istekten gerekli bilgileri alalım.
+    let walletAddress = req.body.walletAddress;
+    let walletChainId = req.body.walletChainId;
+    let farmValueCent = req.body.farmValueCent;
+    let farmDurationDays = req.body.farmDurationDays;
+    let farmPercentPer30Days = req.body.farmPercentPer30Days;
 
-    // Yeni bir çiftlik objesi oluştur
-    const newFarm = {
-        walletAddress: walletAddress || '',
-        walletChainId: walletChainId || 56,
-        farmValueCent: farmValueCent || 0,
-        farmDurationDays: farmDurationDays || 120,
-        farmPercentPer30Days: farmPercentPer30Days || 2500,
-        // ...
-        // Diğer gerekli bilgiler buraya gelebilir
-    };
+    // Kontrol amaçlı konsola bilgileri yazdıralım
+    console.log(`walletAddress: ${walletAddress}`);
+    console.log(`walletChainId: ${walletChainId}`);
+    console.log(`farmValueCent: ${farmValueCent}`);
+    console.log(`farmDurationDays: ${farmDurationDays}`);
+    console.log(`farmPercentPer30Days: ${farmPercentPer30Days}`);
 
-    // Veritabanına yeni bir çiftlik ekleyin (Örnek Firestore)
-    db.collection('farms').add(newFarm)
-        .then(() => {
-            res.status(201).json({message: 'Farm created successfully', farm: newFarm});
-        })
-        .catch(error => {
-            res.status(500).json({message: 'An error occurred: ', error});
-        });
+    // Basit bir yanıt verelim.
+    res.json({status: 'success', message: 'Farm data received'});
 });
 
 const port = process.env.PORT || 3000;
