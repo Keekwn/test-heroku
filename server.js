789ad8d4572848b4692d795a6022234859c792cd
therock34 <ogimelih34@gmail.com>
2023-06-12 14:33:08 +0300

    server.js dosyasına referans düzeltildi

--- server.js
+++ server.js
@@ -7,7 +7,9 @@ const swaggerDocument = YAML.load('./openapi.yaml');
 const app = express();
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
+app.post('/boapi/v1/farm', function(req, res) {
+    // Burada POST isteği için işlem yapılır.
+  });
 // Sunucunuzu başlatın
 const port = process.env.PORT || 5000;  // Heroku, dinlenmesi gereken portu belirtir
 app.listen(port, () => {
