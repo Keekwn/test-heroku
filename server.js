83e8a8e811bb0b9f0e245749cc5e3c31a4dbbe1c
therock34 <ogimelih34@gmail.com>
2023-06-12 15:37:27 +0300

    Dosya güncellemesi

--- server.js
+++ server.js
@@ -1,18 +1,43 @@
-const swaggerUi = require('swagger-ui-express');
-const YAML = require('yamljs');
 const express = require('express');
-const path = require('path');
+const YAML = require('yamljs');
+const swaggerUi = require('swagger-ui-express');
 
-const app = express();
-const port = process.env.PORT || 3000;
-
-// YAML dosyasını yükle
 const swaggerDocument = YAML.load(path.join(__dirname, 'openapi.yaml'));
 
-// Swagger UI'nin /api-docs yolunda görüntülenmesini sağla
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+const app = express();
 
-// Server'ı başlat
-app.listen(port, () => {
-  console.log(`Server ${port} numaralı portta çalışıyor.`);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.post('/boapi/v1/farm', function(req, res) {
+  const data = req.body;  // Gelen veriyi al
+
+  // Verilerin doğruluğunu kontrol et ve gereken işlemleri yap
+  if (validateData(data)) {
+    saveData(data);  // Verileri kaydet
+    makePostRequest(data);  // POST isteği yap
+    res.status(200).json({ message: 'Veri başarıyla işlendi ve POST isteği yapıldı.' });
+  } else {
+    res.status(400).json({ message: 'Geçersiz veri.' });
+  }
 });
+
+// Sunucunuzu başlatın
+const port = process.env.PORT || 5000;  // Heroku, dinlenmesi gereken portu belirtir
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+function validateData(data) {
+  // Gelen verilerin doğruluğunu kontrol et (örneğin, gereken alanlar var mı?)
+  // Doğrulama işlemlerini burada yap ve sonucu döndür
+  // Eğer veriler geçerli ise true, değilse false döndür
+}
+
+function saveData(data) {
+  // Gelen verileri kaydet (örneğin, veritabanına kaydet)
+}
+
+function makePostRequest(data) {
+  // POST isteğini yapacak olan fonksiyonu burada oluştur
+  // Verileri al ve belirtilen hedef URL'ye POST isteğini gerçekleştir
+  // İstek gönderildikten sonra, sonucu kontrol et ve gerekli işlemleri yap
+}
