69dd9810c739931c125ea83bc64ca9eef75ede31
therock34 <ogimelih34@gmail.com>
2023-06-12 15:12:12 +0300

    Dosya güncellemesi

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const swaggerUi = require('swagger-ui-express');
 const YAML = require('yamljs');
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 // YAML dosyasını yükle
-const swaggerDocument = YAML.load('./OneDrive/Masaüstü/server/openapi.yaml');
+const swaggerDocument = YAML.load(path.join(__dirname, 'openapi.yaml'));
 
 // Swagger UI'nin /api-docs yolunda görüntülenmesini sağla
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
