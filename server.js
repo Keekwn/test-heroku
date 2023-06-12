112091bf9c3374654309edca839ac0f37c931226
therock34 <ogimelih34@gmail.com>
2023-06-12 19:25:38 +0300

    First commit

--- server.js
+++ server.js
@@ -1,18 +1,35 @@
 const express = require('express');
-const YAML = require('yamljs');
-const swaggerUi = require('swagger-ui-express');
-const path = require('path');
-
-const swaggerDocument = YAML.load(path.join(__dirname, 'openapi.yaml'));
-
 const app = express();
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-app.post('/boapi/v1/farm', function(req, res) {
-    // Burada POST isteği için işlem yapılır.
+app.use(express.json());
+
+app.post('/boapi/v1/farm', (req, res) => {
+    // Request body'den veriyi al
+    const { walletAddress, walletChainId, farmValueCent, farmDurationDays, farmPercentPer30Days } = req.body;
+
+    // Yeni bir çiftlik objesi oluştur
+    const newFarm = {
+        walletAddress: walletAddress || '',
+        walletChainId: walletChainId || 56,
+        farmValueCent: farmValueCent || 0,
+        farmDurationDays: farmDurationDays || 120,
+        farmPercentPer30Days: farmPercentPer30Days || 2500,
+        // ...
+        // Diğer gerekli bilgiler buraya gelebilir
+    };
+
+    // Veritabanına yeni bir çiftlik ekleyin (Örnek Firestore)
+    db.collection('farms').add(newFarm)
+        .then(() => {
+            res.status(201).json({message: 'Farm created successfully', farm: newFarm});
+        })
+        .catch(error => {
+            res.status(500).json({message: 'An error occurred: ', error});
+        });
 });
 
-const port = process.env.PORT || 5000;
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
